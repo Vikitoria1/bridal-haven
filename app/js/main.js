7853c8aef49b77849c26fd1769bd614bf08dd0fe
Vikitoria1 <grushko13vika@gmail.com>
2025-12-04 19:05:01 +0200

    added reviews, checkout, catalog pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,71 @@
+const viewLine = document.querySelector('.view-mode__btn--line');
+const viewGrid = document.querySelector('.view-mode__btn--grid');
+const viewContainer = document.querySelector('.view-mode__container');
+
+viewGrid.addEventListener('click', () => {
+    viewContainer.classList.add('view-mode__container--grid');
+    viewContainer.classList.remove('view-mode__container--line');
+});
+
+viewLine.addEventListener('click', () => {
+    viewContainer.classList.add('view-mode__container--line');
+    viewContainer.classList.remove('view-mode__container--grid');
+});
+
+
 const swiper = new Swiper('.accessories__slider', {
     slidesPerView: 3,
     spaceBetween: 40,
     loop: true,
     navigation: {
-        nextEl: '.arrow-slider-prew"',
-        prevEl: '.arrow-slider-next',
+        nextEl: '.arrow-slider-next',
+        prevEl: '.arrow-slider-prew',
     },
 });
+
+const swiperReviews = new Swiper('.reviews-slider', {
+    slidesPerView: 12,
+    spaceBetween: 16,
+    loop: true,
+    navigation: {
+        nextEl: '.reviews-slider-next',
+        prevEl: '.reviews-slider-prew',
+    },
+    pagination: {
+        el: ".reviews-slider-pagination",
+        type: "fraction",
+    },
+});
+
+const rangeSlider = document.querySelector('.range__slider');
+const inputMin = document.querySelector('.range__min');
+const inputMax = document.querySelector('.range__max');
+
+noUiSlider.create(rangeSlider, {
+    start: [300, 3000],
+    step: 100,
+    range: {
+        'min': 300,
+        'max': 3000
+    },
+    format: {
+        to: value => Math.round(value),
+        from: value => Number(value)
+    }
+});
+
+rangeSlider.noUiSlider.on('update', (values, handle) => {
+    if (handle === 0) {
+        inputMin.value = values[0]
+    } else {
+        inputMax.value = values[1]
+    }
+})
+
+inputMin.addEventListener('change', () => {
+    rangeSlider.noUiSlider.set([inputMin.value, null])
+})
+
+inputMax.addEventListener('change', () => {
+    rangeSlider.noUiSlider.set([null, inputMax.value])
+})
